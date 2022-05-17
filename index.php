a4cc722277d3fd04306a50e69454e184b49c8c2c
kwimpie <105362709+kwimpie@users.noreply.github.com>
2022-05-17 09:25:48 +0200

    add code

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styling.css">
+</head>
+<body>
+
+<ul>
+    <li><a class="active" href="home.html">Home</a></li>
+    <li><a  href="meedoen.html">Meedoen</a></li>
+    <li><a  href="prijs.html">Prijs</a></li>
+    <li><a  href="info.html">Over</a></li>
+</ul>
+
+
+
+</body>
+</html>
